1f9cb7469a9880ae5d35139b801ca6222455dad8
Avill2 <andrea.villacis@epn.edu.ec>
2018-06-25 11:50:28 -0500

    properties binding and events

--- App/src/app/app.component.ts
+++ App/src/app/app.component.ts
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  //interpolacion
+  titulo = 'WEB';
+  usuario= {nombre:'andrea', apellido:'villacis'};
+  arreglonumeros=[5,4,3,2,1];
+  mostrartitulo=true;
+  imgurl= "http://jsequeiros.com/sites/default/files/imagen-cachorro-comprimir.jpg";
+  imagenwidth="200";
+
+  html = `
+  <h1>HOLA</h1>
+  <p>Hola mundo</p>
+  <img src="${this.imgurl}" alt="">
+`;
+//funciones en nuestra clase
+  aumentarwidth(){
+    this.imagenwidth += 10;
+  }
+  aumentarWidth(evento) {
+    console.log('evento', evento);
+    this.imagenwidth += 10;
+  }
+
+  restarWidth() {
+    this.imagenwidth -= 10;
+  }
+
 }
+
+
+
